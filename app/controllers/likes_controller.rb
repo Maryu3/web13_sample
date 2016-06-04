54df09ccb0c90b17523f18ec5e522f18e90897b1
maryu <beskey4646@gmail.com>
2016-06-04 11:13:53 +0000

    first commit

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,21 @@
+class LikesController < ApplicationController
+	def create
+		@post = Post.find(params[:post_id])
+		@like = current_user.likes.build(post_id: @post.id)
+
+		if @like.save
+			redirect_to posts_path, notice:"いいねできました"
+		else
+			redirect_to posts_path, alert:"いいねできませんでした"
+		end
+	end
+
+	def destroy
+		@like = current_user.likes.find_by!(post_id: params[:post_id])
+		@like.destroy
+		redirect_to posts_path, notice: "お気に入りを削除しました"
+		
+	end
+
+
+end
